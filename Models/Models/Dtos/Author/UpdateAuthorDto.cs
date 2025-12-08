8b5f463bf73a674a623ad2eab8242a4fb1935fe1
anacarolina-py <anacarolinaalvez_@hotmail.com>
2025-12-07 22:54:25 -0300

    Alteracao update

--- Models/Models/Dtos/Author/UpdateAuthorDto.cs
+++ Models/Models/Dtos/Author/UpdateAuthorDto.cs
@@ -5,23 +5,22 @@ namespace Models.Models.Dtos.Author;
 public class UpdateAuthorDto
 {
     [BsonElement("name")]
-    public string Name { get; init; } = string.Empty;
+    public string? Name { get; init; } 
 
     [BsonElement("title")]
-    public string Title { get; init; } = string.Empty;
+    public string? Title { get; init; } 
 
     [BsonElement("image")]
-    public string Image { get; init; } = string.Empty;
+    public string? Image { get; init; } 
 
     [BsonElement("bio")]
-    public string Bio { get; init; } = string.Empty;
+    public string? Bio { get; init; } 
 
     [BsonElement("url")]
-    public string Url { get; init; } = string.Empty;
-
+    public string? Url { get; init; } 
     [BsonElement("email")]
-    public string Email { get; init; } = string.Empty;
+    public string? Email { get; init; } 
 
     [BsonElement("type")]
-    public byte Type { get; init; } = 1;
+    public byte? Type { get; init; } = 1;
 }
